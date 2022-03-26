e92b7f10b9527b1d3013936b337a1358d625ac8f
Guillaume <g.wilmot@groupeonepoint.com>
2022-03-26 15:53:18 +1100

    Feature Home: - Add program mesocycle block type to program chart

--- app/src/main/java/com/guillaumewilmot/swoleai/modules/home/dashboard/HomeDashboardFragment.kt
+++ app/src/main/java/com/guillaumewilmot/swoleai/modules/home/dashboard/HomeDashboardFragment.kt
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView
 import autodispose2.androidx.lifecycle.autoDispose
 import com.github.mikephil.charting.charts.CombinedChart.DrawOrder
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.CombinedData
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.formatter.IFillFormatter
@@ -34,6 +35,7 @@ import com.guillaumewilmot.swoleai.util.extension.withSpans
 import com.guillaumewilmot.swoleai.view.EqualSpacingItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.lang.Float.max
 import java.util.*
 
 
@@ -61,6 +63,7 @@ class HomeDashboardFragment : ParentFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
         ui()
         setupFatigueChart()
         setupProgramChart()
@@ -69,10 +72,26 @@ class HomeDashboardFragment : ParentFragment() {
          * Fatigue chart
          */
 
+        viewModel.currentFatigueValue
+            .autoDispose(this, Lifecycle.Event.ON_PAUSE)
+            .subscribe { fatigue ->
+                binding?.fatigueRatingValue?.text = fatigue.toString()
+            }
+
         viewModel.fatigueChartState
             .autoDispose(this, Lifecycle.Event.ON_PAUSE)
             .subscribe { state ->
                 binding?.fatigueChart?.apply {
+                    val padding = (state.dataset.yMax - state.dataset.yMin) * 0.5f
+
+                    axisLeft.axisMaximum = state.dataset.yMax + padding
+                    axisLeft.axisMinimum = max(0f, state.dataset.yMin - padding)
+
+                    xAxis.axisMaximum = state.dataset.xMax
+                    xAxis.axisMinimum = state.dataset.xMin
+
+                    xAxis.valueFormatter = IndexAxisValueFormatter(state.xAxisValues)
+
                     data = LineData(state.dataset.apply {
                         fillFormatter = IFillFormatter { _, _ ->
                             axisLeft?.axisMinimum ?: 0f
@@ -83,9 +102,8 @@ class HomeDashboardFragment : ParentFragment() {
                         binding?.fatigueChart?.xAxis?.addLimitLine(limitLine)
                     }
 
-                    xAxis?.valueFormatter = IndexAxisValueFormatter(state.xAxisValues)
-
                     invalidate()
+                    visibility = View.VISIBLE
                 }
             }
 
@@ -107,16 +125,23 @@ class HomeDashboardFragment : ParentFragment() {
             .autoDispose(this, Lifecycle.Event.ON_PAUSE)
             .subscribe { state ->
                 binding?.programChart?.apply {
+                    legend.setCustom(state.legend)
+                    legend.isWordWrapEnabled = true
+
+                    axisLeft.axisMaximum = max(state.intensityData.yMax, state.volumeData.yMax)
+                    axisLeft.axisMinimum = -1f //Avoid bar borders getting cutoff
+
                     data = CombinedData().apply {
                         setData(state.volumeData)
                         setData(state.intensityData)
                     }
 
-                    //This allows first and last bar to not be half cutoff
-                    xAxis.axisMinimum = -barData.barWidth / 2f
-                    xAxis.axisMaximum = state.volumeData.xMax + barData.barWidth / 2f
+                    //This allows first and last bar to not be half cutoff + borders
+                    xAxis.axisMinimum = -barData.barWidth / 2f - 1f
+                    xAxis.axisMaximum = state.volumeData.xMax + barData.barWidth / 2f + 1f
 
                     invalidate()
+                    visibility = View.VISIBLE
                 }
             }
     }
@@ -127,8 +152,7 @@ class HomeDashboardFragment : ParentFragment() {
      * Includes a fake fatigue chart
      */
 
-
-    private fun ui() {
+    private fun setupToolbar() {
         binding?.toolbarLayout?.toolbarContent?.apply {
             this.iconAction.visibility = View.VISIBLE
             this.iconAction.setImageDrawable(
@@ -143,7 +167,9 @@ class HomeDashboardFragment : ParentFragment() {
                 ForegroundColorSpan(this.iconAction.context.getColor(R.color.secondary))
             )
         }
+    }
 
+    private fun ui() {
         binding?.fatigueRatingValue?.text = 20.00.toString()
 
         binding?.weekTitle?.text = SpannableString("Hypertrophy\nMarch 21st\nWeek 3").withSpans(
@@ -174,56 +200,58 @@ class HomeDashboardFragment : ParentFragment() {
 
     private fun setupFatigueChart() {
         binding?.fatigueChart?.let { chart ->
+            val textColor = chart.context.getColor(R.color.textPrimary)
+
+            chart.visibility = View.INVISIBLE
             chart.setTouchEnabled(false)
             chart.setPinchZoom(false)
             chart.isDragEnabled = false
             chart.description.isEnabled = false
             chart.legend.isEnabled = false
             chart.minOffset = 0f //Remove start end padding
-            chart.extraLeftOffset = 1f //Avoid cutting left axis off
-            chart.extraRightOffset = 1f //Avoid cutting right axis off
+            chart.extraTopOffset = 5f
 
-            chart.axisLeft.axisMaximum = 25f
-            chart.axisLeft.axisMinimum = 5f
+            chart.axisLeft.isEnabled = true
             chart.axisLeft.setDrawGridLines(false)
             chart.axisLeft.setDrawAxisLine(false)
             chart.axisLeft.setDrawLabels(false)
 
-            chart.axisRight.isEnabled = false
+            chart.axisRight.isEnabled = true
+            chart.axisRight.setDrawGridLines(false)
+            chart.axisRight.setDrawAxisLine(false)
+            chart.axisRight.setDrawLabels(true)
+            chart.axisRight.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+            chart.axisRight.textColor = textColor
 
-            val textColor = chart.context.getColor(R.color.textPrimary)
             chart.xAxis.textColor = textColor
             chart.xAxis.axisLineColor = textColor
             chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
             chart.xAxis.setDrawGridLines(false)
             chart.xAxis.setDrawAxisLine(true)
-            chart.xAxis.axisMaximum = 3f
-            chart.xAxis.axisMinimum = 0f
-            chart.xAxis.axisLineWidth = activity.pixelToDp(1f)
+            chart.xAxis.axisLineWidth = activity.pixelToDp(2f)
             chart.xAxis.setAvoidFirstLastClipping(true)
-            chart.xAxis.setDrawLimitLinesBehindData(true)
+            chart.xAxis.setDrawLimitLinesBehindData(false)
         }
     }
 
     private fun setupProgramChart() {
         binding?.programChart?.let { chart ->
+            chart.visibility = View.INVISIBLE
             chart.setTouchEnabled(false)
             chart.setPinchZoom(false)
             chart.isDragEnabled = false
             chart.description.isEnabled = false
-            chart.legend.isEnabled = false
-            chart.minOffset = 0f //Remove start end padding
-            chart.extraLeftOffset = 1f //Avoid cutting left axis off
-            chart.extraRightOffset = 1f //Avoid cutting right axis off
+            chart.legend.isEnabled = true
+            chart.legend.textColor = chart.context.getColor(R.color.textSecondary)
+            chart.legend.xOffset = 15f
 
-            chart.axisLeft.axisMaximum = 8f
-            chart.axisLeft.axisMinimum = 0f
+            chart.minOffset = 0f //Remove start end padding
+
             chart.axisLeft.setDrawGridLines(false)
             chart.axisLeft.setDrawAxisLine(false) //true
             chart.axisLeft.setDrawLabels(false)
 
             chart.axisRight.isEnabled = false
-
             chart.xAxis.isEnabled = false
 
             chart.drawOrder = arrayOf(
--- app/src/main/java/com/guillaumewilmot/swoleai/modules/home/dashboard/HomeDashboardViewModel.kt
+++ app/src/main/java/com/guillaumewilmot/swoleai/modules/home/dashboard/HomeDashboardViewModel.kt
@@ -1,8 +1,11 @@
 package com.guillaumewilmot.swoleai.modules.home.dashboard
 
 import android.app.Application
+import android.graphics.DashPathEffect
 import android.view.View
 import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LegendEntry
 import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.data.*
 import com.guillaumewilmot.swoleai.R
@@ -51,7 +54,7 @@ class HomeDashboardViewModel @Inject constructor(
             listOf(
                 FatigueValue(now.minusDays(3), 17.500f),
                 FatigueValue(now.minusDays(2), 18.200f),
-                FatigueValue(now.minusDays(1), 20.300f),
+                FatigueValue(now.minusDays(1), 25.300f),
                 FatigueValue(now, 21.000f)
             )
         )
@@ -93,6 +96,12 @@ class HomeDashboardViewModel @Inject constructor(
             )
         }
     }
+
+    val currentFatigueValue: Flowable<Float> = _fatigueData.map { fatigueData ->
+        fatigueData.lastOrNull()?.value ?: 0f
+    }
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 
     val fatigueChartState: Flowable<FatigueChartState> = Flowable.zip(
         _fatigueChartDataSet,
@@ -168,36 +177,69 @@ class HomeDashboardViewModel @Inject constructor(
         val volume: Float
     )
 
+    data class ProgramBlock(
+        val type: BlockType,
+        val weeks: List<ProgramWeekValue>
+    ) {
+        enum class BlockType {
+            HYPERTROPHY,
+            STRENGTH,
+            PEAKING
+        }
+    }
+
     //FIXME : TMP hardcoded data for fatigue chart
-    private val _programWeeksData: Flowable<List<ProgramWeekValue>> = Flowable.create({
+    private val _programBlocksData: Flowable<List<ProgramBlock>> = Flowable.create({
         it.onNext(
             listOf(
-                ProgramWeekValue(Date().minusDays(2 * 7), 0.5f, 1.5f),
-                ProgramWeekValue(Date().minusDays(1 * 7), 2.5f, 2f),
-                ProgramWeekValue(Date(), 3f, 3f),
-                ProgramWeekValue(Date().plusDays(1 * 7), 2.5f, 4f),
-
-                ProgramWeekValue(Date().plusDays(2 * 7), 1.8f, 1f),
-                ProgramWeekValue(Date().plusDays(3 * 7), 2f, 2f),
-                ProgramWeekValue(Date().plusDays(4 * 7), 2.8f, 3f),
-                ProgramWeekValue(Date().plusDays(5 * 7), 2.5f, 4f),
-                ProgramWeekValue(Date().plusDays(6 * 7), 1.8f, 1f),
-
-                ProgramWeekValue(Date().plusDays(7 * 7), 4f, 3.5f),
-                ProgramWeekValue(Date().plusDays(8 * 7), 4.5f, 2.5f),
-                ProgramWeekValue(Date().plusDays(9 * 7), 5f, 1.5f),
-                ProgramWeekValue(Date().plusDays(10 * 7), 4f, 1f),
+                ProgramBlock(
+                    ProgramBlock.BlockType.HYPERTROPHY,
+                    listOf(
+                        ProgramWeekValue(Date().minusDays(2 * 7), 0.5f, 1.5f),
+                        ProgramWeekValue(Date().minusDays(1 * 7), 2.5f, 2f),
+                        ProgramWeekValue(Date(), 3f, 3f),
+                        ProgramWeekValue(Date().plusDays(1 * 7), 2.5f, 4f),
+                        ProgramWeekValue(Date().plusDays(2 * 7), 1.8f, 1f)
+                    )
+                ),
+                ProgramBlock(
+                    ProgramBlock.BlockType.HYPERTROPHY,
+                    listOf(
+                        ProgramWeekValue(Date().plusDays(3 * 7), 2f, 2f),
+                        ProgramWeekValue(Date().plusDays(4 * 7), 2.8f, 3f),
+                        ProgramWeekValue(Date().plusDays(5 * 7), 2.5f, 4f),
+                        ProgramWeekValue(Date().plusDays(6 * 7), 1.8f, 1f)
+                    )
+                ),
+                ProgramBlock(
+                    ProgramBlock.BlockType.STRENGTH,
+                    listOf(
+                        ProgramWeekValue(Date().plusDays(7 * 7), 4f, 3.5f),
+                        ProgramWeekValue(Date().plusDays(8 * 7), 4.5f, 2.5f),
+                        ProgramWeekValue(Date().plusDays(9 * 7), 5f, 1.5f),
+                        ProgramWeekValue(Date().plusDays(10 * 7), 4f, 1f)
+                    )
+                ),
+                ProgramBlock(
+                    ProgramBlock.BlockType.PEAKING,
+                    listOf(
+                        ProgramWeekValue(Date().plusDays(11 * 7), 7.5f, 2.5f),
+                        ProgramWeekValue(Date().plusDays(12 * 7), 8f, 2f),
+                        ProgramWeekValue(Date().plusDays(13 * 7), 4f, 1.5f),
+                    )
+                )
             )
         )
     }, BackpressureStrategy.LATEST)
 
-
     //FIXME : TMP hardcoded data for blueprints */
     private val _programChartIntensityDataSet: Flowable<LineData> =
-        _programWeeksData.map { programWeeksData ->
+        _programBlocksData.map { programBlocks ->
             LineData().apply {
                 addDataSet(LineDataSet(
-                    programWeeksData.mapIndexed { i, programWeek ->
+                    programBlocks.flatMap {
+                        it.weeks
+                    }.mapIndexed { i, programWeek ->
                         Entry(i.toFloat(), programWeek.intensity)
                     },
                     ""
@@ -213,19 +255,8 @@ class HomeDashboardViewModel @Inject constructor(
         }
 
     private val _programChartVolumeDataSet: Flowable<BarData> =
-        _programWeeksData.map { programWeeksData ->
+        _programBlocksData.map { programBlocks ->
             BarData().apply {
-                val now = Date()
-                val pastWeeks = programWeeksData.filter {
-                    it.date.before(now) && it.date.isSameWeek(now).not()
-                }
-                val currentWeek = programWeeksData.filter {
-                    it.date.isSameWeek(now)
-                }
-                val futureWeeks = programWeeksData.filter {
-                    it.date.after(now) && it.date.isSameWeek(now).not()
-                }
-
                 var index = 0
                 fun createDataset(
                     weeksData: List<ProgramWeekValue>,
@@ -243,23 +274,112 @@ class HomeDashboardViewModel @Inject constructor(
                     this.barBorderColor = barBorderColor
                 }
 
-                val pastColor = application.getColor(R.color.mainLight)
-                val currentColor = application.getColor(R.color.main)
-                val futureColor = application.getColor(R.color.transparent)
-                val borderColor = application.getColor(R.color.main)
-                addDataSet(createDataset(pastWeeks, pastColor, borderColor))
-                addDataSet(createDataset(currentWeek, currentColor, borderColor))
-                addDataSet(createDataset(futureWeeks, futureColor, borderColor))
+                val now = Date()
+                programBlocks.forEach { programBlock ->
+                    val currentColor = application.getColor(
+                        when (programBlock.type) {
+                            ProgramBlock.BlockType.HYPERTROPHY -> R.color.hypertrophy
+                            ProgramBlock.BlockType.STRENGTH -> R.color.strength
+                            ProgramBlock.BlockType.PEAKING -> R.color.peaking
+                        }
+                    )
+
+                    //Past
+                    programBlock.weeks.filter {
+                        it.date.before(now) && it.date.isSameWeek(now).not()
+                    }.takeIf {
+                        it.isNotEmpty()
+                    }?.let {
+                        val pastColor = application.getColor(
+                            when (programBlock.type) {
+                                ProgramBlock.BlockType.HYPERTROPHY -> R.color.hypertrophyPast
+                                ProgramBlock.BlockType.STRENGTH -> R.color.strengthPast
+                                ProgramBlock.BlockType.PEAKING -> R.color.peakingPast
+                            }
+                        )
+                        addDataSet(createDataset(it, pastColor, currentColor))
+                    }
+
+                    //Current
+                    programBlock.weeks.filter {
+                        it.date.isSameWeek(now)
+                    }.takeIf {
+                        it.isNotEmpty()
+                    }?.let {
+                        addDataSet(createDataset(it, currentColor, currentColor))
+                    }
+
+                    //Future
+                    programBlock.weeks.filter {
+                        it.date.after(now) && it.date.isSameWeek(now).not()
+                    }.takeIf {
+                        it.isNotEmpty()
+                    }?.let {
+                        val futureColor = application.getColor(R.color.transparent)
+                        addDataSet(createDataset(it, futureColor, currentColor))
+                    }
+                }
 
                 barWidth = 0.8f
             }
         }
 
+    private val _programChartLegend: Flowable<List<LegendEntry>> =
+        _programBlocksData.map { programBlocks ->
+            listOfNotNull(
+                programBlocks.find {
+                    it.type == ProgramBlock.BlockType.HYPERTROPHY
+                }?.let {
+                    LegendEntry(
+                        application.getString(R.string.app_home_dashboard_program_summary_chart_legend_hypertrophy),
+                        Legend.LegendForm.SQUARE,
+                        5f,
+                        Float.NaN,
+                        null,
+                        application.getColor(R.color.hypertrophy)
+                    )
+                },
+                programBlocks.find {
+                    it.type == ProgramBlock.BlockType.STRENGTH
+                }?.let {
+                    LegendEntry(
+                        application.getString(R.string.app_home_dashboard_program_summary_chart_legend_strength),
+                        Legend.LegendForm.SQUARE,
+                        5f,
+                        Float.NaN,
+                        null,
+                        application.getColor(R.color.strength)
+                    )
+                },
+                programBlocks.find {
+                    it.type == ProgramBlock.BlockType.PEAKING
+                }?.let {
+                    LegendEntry(
+                        application.getString(R.string.app_home_dashboard_program_summary_chart_legend_peaking),
+                        Legend.LegendForm.SQUARE,
+                        5f,
+                        Float.NaN,
+                        null,
+                        application.getColor(R.color.peaking)
+                    )
+                },
+                LegendEntry(
+                    application.getString(R.string.app_home_dashboard_program_summary_chart_legend_intensity),
+                    Legend.LegendForm.LINE,
+                    Float.NaN,
+                    1f,
+                    DashPathEffect(floatArrayOf(5f, 5f), 0f),
+                    application.getColor(R.color.textTertiary)
+                ),
+            )
+        }
+
     val programChartState: Flowable<ProgramChartState> = Flowable.zip(
         _programChartVolumeDataSet,
         _programChartIntensityDataSet,
-    ) { volumeDataset, intensityDataset ->
-        ProgramChartState(volumeDataset, intensityDataset)
+        _programChartLegend
+    ) { volumeDataset, intensityDataset, programChartLegend ->
+        ProgramChartState(volumeDataset, intensityDataset, programChartLegend)
     }
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
@@ -267,5 +387,6 @@ class HomeDashboardViewModel @Inject constructor(
     data class ProgramChartState(
         val volumeData: BarData,
         val intensityData: LineData,
+        val legend: List<LegendEntry>,
     )
 }
